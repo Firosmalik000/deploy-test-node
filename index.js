9927a387aeff0f446f0a277ff7cc8fb13d8a8560
firosmalik000 <firosmalik44@gmail.com>
2024-08-19 06:12:40 +0700

    manage env file

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const itemRoute = require('./routes/itemRoute');
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-// Route untuk API items
 app.use('/api/items', itemRoute);
 
-// Route sambutan dipindahkan ke bawah agar tidak menghalangi route lainnya
 app.use('/', (req, res) => {
   return res.status(200).send('Welcome to Node.js programs');
 });
 
-// Koneksi ke MongoDB
 mongoose
-  .connect(
-    'mongodb://root:root123@ac-ufjoenx-shard-00-00.wv5qisj.mongodb.net:27017,ac-ufjoenx-shard-00-01.wv5qisj.mongodb.net:27017,ac-ufjoenx-shard-00-02.wv5qisj.mongodb.net:27017/test-deploy?ssl=true&replicaSet=atlas-xeklsr-shard-0&authSource=admin&retryWrites=true&w=majority'
-  )
+  .connect(process.env.MONGODB_URL)
   .then(() => {
     console.log('connected to db');
   })
@@ -26,6 +24,6 @@ mongoose
   });
 
 // Start server
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
   console.log('listening on port 5000');
 });
