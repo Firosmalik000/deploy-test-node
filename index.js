40a94e57072eb2bed4582af14fc15aa4c334c16f
firosmalik000 <firosmalik44@gmail.com>
2024-08-21 14:28:46 +0700

    debugging2

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
       mongoUrl: process.env.MONGODB_URL,
       collectionName: 'sessions',
     }),
-    cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24, httpOnly: true },
+    cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24, httpOnly: true, sameSite: 'None' },
   })
 );
 app.use(
